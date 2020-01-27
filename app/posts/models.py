ab0169fe487ae0904ce0d9d24173acafe32e3642
Hyojin Kwon <raccoonhj33@gmail.com>
2020-01-27 20:00:02 +0900

    200127 defined model Post in posts app

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# 모델 정의하기
+class Post(models.Model):
+    # ForeignKey 다른 모델에 대한 링크
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # TextField 글자 수 제한 없는 텍스트
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    # Post 모델의 author, created 속성을 string 형태로 호출
+    def __str__(self):
+        return f'{self.author} | {self.created}'
