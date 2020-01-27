8df8acb697d7a366c591ccb3eaa5f29c7659a361
Hyojin Kwon <raccoonhj33@gmail.com>
2020-01-27 20:40:31 +0900

    200127 practiced showing post list template in post-list.html using for loop

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from posts.models import Post
+
 
 def post_list(request):
     # request를 넘겨받아 render 호출
     # posts/post_list.html 템플릿을 보여줌
-    return render(request, 'posts/post_list.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/post_list.html', context)
